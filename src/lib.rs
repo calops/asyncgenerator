8451c5bddcf54a5714d5f773d084854fa7804fca
Rémi Labeyrie <rlabeyrie@gmail.com>
2017-09-01 19:13:09 +0200

    Code complete, lifetime issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,57 @@
 #![feature(generators, generator_trait, conservative_impl_trait, proc_macro)]
 
 extern crate futures_await as futures;
+extern crate crossbeam;
 
 use std::ops::Generator;
+use std::ops::GeneratorState;
 use futures::prelude::*;
+use crossbeam::sync::chase_lev;
 
 struct AsyncGenerator<T>
 where T: Generator
 {
     generator: T,
-    values: Vec<T::Yield>
+    producer: chase_lev::Worker<GeneratorState<T::Yield, T::Return>>,
+    consumer: chase_lev::Stealer<GeneratorState<T::Yield, T::Return>>
 }
 
 impl<T> AsyncGenerator<T>
-where T: Generator + 'static
+where T: Generator
 {
     fn new(generator: T) -> Self {
-        AsyncGenerator {
+        let (mut producer, consumer) = chase_lev::deque();
+        let mut async_gen = AsyncGenerator {
             generator: generator,
-            values: Vec::new()
+            producer: producer,
+            consumer: consumer
+        };
+        async_gen.populate();
+        async_gen
+    }
+
+    #[async]
+    fn resume(&mut self) -> Result<Option<T::Yield>, ()> {
+        loop {
+            match self.consumer.steal() {
+                chase_lev::Steal::Data(data) => match data {
+                    GeneratorState::Yielded(value) => return Ok(Some(value)),
+                    GeneratorState::Complete(_) => return Ok(None)
+                }
+            }
         }
     }
 
     #[async]
-    fn resume(&mut self) -> Result<T::Yield, T::Return> {
-        unimplemented!()
+    fn populate(&mut self) -> Result<(), ()> {
+        loop {
+            let next_item = self.generator.resume();
+            self.producer.push(next_item);
+            match next_item {
+                GeneratorState::Complete(_) => break
+            }
+        }
+        Ok(())
     }
 }
 
@@ -35,7 +62,7 @@ where T: Generator
 }
 
 impl<T> IntoAsync<T> for T
-where T: Generator + 'static
+where T: Generator
 {
     fn into_async(self) -> AsyncGenerator<T> {
         AsyncGenerator::new(self)
@@ -43,7 +70,7 @@ where T: Generator + 'static
 }
 
 impl<T> Iterator for AsyncGenerator<T>
-where T: Generator + 'static
+where T: Generator
 {
     type Item = T::Yield;
 
@@ -52,7 +79,8 @@ where T: Generator + 'static
             await!(self.resume())
         };
         match future.wait() {
-            Ok(next_value) => Some(next_value),
+            Ok(Some(next_value)) => Some(next_value),
+            Ok(None) => None,
             Err(_) => None
         }
     }
